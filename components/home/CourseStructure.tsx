6383c2dea722b7265cb89c7e71a43b5d16531a43
Almaash <alamalmaash@gmail.com>
2025-09-14 14:41:01 +0530

    aaaaaaaaaa

--- components/home/CourseStructure.tsx
+++ components/home/CourseStructure.tsx
@@ -93,7 +93,7 @@ export default function CourseStructure() {
         </div>
 
         {/* Courses Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-12 p-10">
+<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-12 px-2 sm:px-4 md:px-6 lg:px-10">
           {courses.map((course) => (
             <div
               key={course.id}
